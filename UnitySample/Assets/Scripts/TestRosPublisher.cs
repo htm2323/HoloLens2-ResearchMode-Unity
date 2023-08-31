b689f9b90dbc7a67f7d6f063e5a2823c0b1c2b82
htm2323 <83809067+htm2323@users.noreply.github.com>
2023-08-31 18:26:28 +0900

    [feat]毎フレーム特定の画像をpublishする機能を実装、動作を確認

---
+++ UnitySample/Assets/Scripts/TestRosPublisher.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class TestRosPublisher : UnityPublisher<MessageTypes.Sensor.CompressedImage>
+    {
+        public string FrameId = "Hololens";
+        public Texture2D image;
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+            //InitializeMessage();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var message = new MessageTypes.Sensor.CompressedImage
+            {
+                data = image.EncodeToPNG()
+            };
+
+            Publish(message);
+
+            Debug.Log("publishing message");
+
+            /*if (Input.GetKeyDown(KeyCode.Space))
+            {
+                var message = new MessageTypes.Sensor.CompressedImage
+                {
+                    data = image.EncodeToPNG()
+                };
+
+                Publish(message);
+
+                Debug.Log("publishing message");
+            }*/
+        }
+
+        private void InitializeMessage()
+        {
+
+        }
+
+        private void UpdateMessage()
+        {
+
+        }
+    }
+}
